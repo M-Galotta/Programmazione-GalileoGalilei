cbdd70823943c0c12e4d96f3a5d62889c897b3be
M-Galotta <m.galotta74@gmail.com>
2024-11-14 10:50:23 +0000

    while e cnt

---
+++ Informatica/Terza/Iterazioni/Es2_20241114.c
@@ -0,0 +1,19 @@
+/*
+   LEGGERE UNA SEQUENZA DI NUMERI, VISUALIZZARLA. 
+   SI TERMINA NON APPENA ARRIVA UNO 0 
+*/
+#include <stdio.h>
+
+int main(){
+    int numero;
+
+    printf("Inserisci un numero: ");
+    scanf("%d", &numero);
+
+    while( numero!=0){
+        printf("Il valore inserito è: %d\n", numero);
+        printf("Inserisci un numero: ");
+        scanf("%d", &numero);
+    }
+
+}
