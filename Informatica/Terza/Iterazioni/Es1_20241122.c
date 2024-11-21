889616e1c5f1e0ffae56081e61b701c6dcae1671
M-Galotta <m.galotta74@gmail.com>
2024-11-21 10:46:12 +0000

    divisori

---
+++ Informatica/Terza/Iterazioni/Es1_20241122.c
@@ -0,0 +1,18 @@
+/* Esercizio
+Dato un numero stampare tutto i suoi divisori
+*/
+#include <stdio.h>
+int main(){
+    int numero, divisore;
+
+    divisore=2;
+    printf("Inserisci un numero: ");
+    scanf("%d", &numero);
+
+    while(divisore<=numero/2){
+        if(numero%divisore==0)
+            printf("%d è un divisore di %d\n", divisore, numero);
+        
+        divisore++;
+    }
+}
