bf3440f6b09971e8ebbaeb0ef38ea8f485731714
M-Galotta <m.galotta74@gmail.com>
2025-02-10 10:34:39 +0000

    media

---
+++ Informatica/Terza/funzioni/Cerchio.c
@@ -0,0 +1,4 @@
+/* Dato il raggio calcolare la circonferenza e l'area del cerchio 
+    Eseguire il passaggio dei parametri per indirizzo   
+    c= 2*PI*raggio   PI=3.14     Ac=PI*raggio*raggio    */
+#include <stdio.h>
---
+++ Informatica/Terza/funzioni/Fatt_Puntatori.c
@@ -0,0 +1,26 @@
+/* Calcolare il fattoriale di un numero 
+  Es: 7!= 1*2*3*4*5*6*7     */
+#include <stdio.h>
+void fattoriale(int *_n,long *_fatt);
+int main(){
+    int n=0;
+    long fatt=1;
+    
+    do{
+        printf("inserisci un numero: ");
+        scanf("%d",&n);
+    }while(n<=0);
+
+    fattoriale(&n,&fatt);//   n parametro effettivo
+    printf("il fattoriale di %d è %ld", n, fatt);
+
+}
+void fattoriale(int *_n,long *_fatt){
+   
+    for(int i=1; i<=*_n; i++){
+        *_fatt*=i;
+    }
+    
+}
+
+
