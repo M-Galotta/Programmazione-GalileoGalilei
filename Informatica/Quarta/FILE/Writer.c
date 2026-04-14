338769a6b32ec1cbf3d8300985d4430494733d6a
M-Galotta <m.galotta74@gmail.com>
2026-04-14 07:14:37 +0000

    ...

--- Informatica/Quarta/FILE/Writer.c
+++ Informatica/Quarta/FILE/Writer.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    FILE *fp = fopen("/workspaces/Programmazione-GalileoGalilei/Informatica/Quarta/FILE/numeri.dat", "wb");
+    FILE *fp = fopen("numeri.dat", "wb");
     int numeri[3] = {10, 20, 30};
 	
 	if (fp == NULL) {
